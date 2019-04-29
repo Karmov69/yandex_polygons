168542df5addf34a431b9e4964282e95ddae009f
Karmov <a.karmov@pugofka.com>
2019-04-29 15:56:36 +0300

    feat

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function init() {
   // В режиме добавления новых вершин меняем цвет обводки многоугольника.
   var stateMonitor = new ymaps.Monitor(myPolygon.editor.state);
   stateMonitor.add("drawing", function(newValue) {
-    console.log(myMap.geoObjects);
+    console.log(myMap.geoObject.geometry.getCoordinates());
     myPolygon.options.set("strokeColor", newValue ? "#FF0000" : "#0000FF");
   });
 
